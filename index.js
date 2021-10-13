6a849ee21ceaf7775881d854bb0c00fd11538b9e
Ely Karr <elykarr@gmail.com>
2021-10-13 14:12:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+
+
+function submitData(inputName, inputEmail) {
+    fetch("http://localhost:3000/users") {
+        method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+                Accept: "application/json",
+  },
+        body: JSON.stringify({
+            name: inputName,
+            email: inputEmail,
+        }),
+    
+});
+
+
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     });
+
